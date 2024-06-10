87451f55d746992930b76c0b6c5298523548b985
juliacheperis13 <35917877+juliacheperis13@users.noreply.github.com>
2024-06-10 11:30:42 +0000

    Deploying to gh-pages from @ juliacheperis13/goit-advancedjs-hw-01@537b0a8c1dcf071566e9edcecacdbe7ed538ef45 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */import{P as r,t as o}from"./assets/vendor-1e54b975.js";const a=document.querySelector("iframe"),e=new r(a),m=1e3,i=t=>{localStorage.setItem("videoplayer-current-time",t.seconds)};localStorage.getItem("videoplayer-current-time")&&e.setCurrentTime(localStorage.getItem("videoplayer-current-time"));e.on("timeupdate",o(i,m));
+//# sourceMappingURL=commonHelpers2.js.map
